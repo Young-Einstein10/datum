56dee74e79d6dc58dbbd760d8221c79c20df90ea
Young-Einstein10 <aabdulrahmanyusuf@gmail.com>
2022-08-27 08:06:16 +0100

    chore: fix increment/decrement

--- src/app.ts
+++ src/app.ts
@@ -76,7 +76,7 @@ const fetchPageData = async (page = 1) => {
 
 const paginate = (type = "next") => {
   if (type === "prev") {
-    currentPage -= 1;
+    currentPage -= 2;
 
     if (tableData[currentPage].length) {
       updateTableUI(currentPage);
